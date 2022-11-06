563bf556f7f2805cb2538e561552d83d835ad39d
Hejiko Hejio <hejyll@gmail.com>
2022-11-06 16:51:53 +0900

    Add _version.py

--- src/jpradio_recorder/__init__.py
+++ src/jpradio_recorder/__init__.py
@@ -1,3 +1,4 @@
+from ._version import __version__
 from .database import Database  # NOQA
 from .query import ProgramQuery, ProgramQueryList  # NOQA
 from .recorder import RecordedProgram, Recorder  # NOQA
