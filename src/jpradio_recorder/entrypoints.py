61b7e51167a1bf9187bd134f615f9671cb86277d
Hejiko Hejio <hejyll@gmail.com>
2022-11-06 16:28:45 +0900

    Add args

--- src/jpradio_recorder/entrypoints.py
+++ src/jpradio_recorder/entrypoints.py
@@ -1,15 +1,21 @@
-from typing import List
+from typing import List, Optional
 
 from .query import ProgramQueryList
 from .recorder import RecordedProgram, Recorder
 
 
 def record(
-    media_root: str,
     queries_path: str,
+    media_root: Optional[str] = None,
+    platform_config_path: Optional[str] = None,
+    database_host: Optional[str] = None,
 ) -> List[RecordedProgram]:
     queries = ProgramQueryList.from_json(queries_path)
-    with Recorder(media_root=media_root) as recorder:
+    with Recorder(
+        media_root=media_root,
+        platform_config_path=platform_config_path,
+        database_host=database_host,
+    ) as recorder:
         recorder.fetch_programs()
         recorder.reserve_programs(queries)
         programs = recorder.record_programs()
