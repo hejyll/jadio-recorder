af59b0182c2d7e63b20bcdaca980fcf7c80979df
Hejiko Hejio <hejyll@gmail.com>
2022-11-06 19:34:14 +0900

    Fix database-host for docker-env

--- docker/jpradio-recorder.sh
+++ docker/jpradio-recorder.sh
@@ -4,4 +4,4 @@ jpradio-recorder \
     --queries-path /data/jpradio-recorder/queries.json \
     --media-root /data/media \
     --platform-config-path /data/jpradio-recorder/config.json \
-    --database-host "mongodb://localhost:27017/"
+    --database-host "mongodb://docker_mongo_1:27017/"
