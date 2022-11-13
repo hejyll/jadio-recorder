c60fddddf0a3af7ab67350cb753ecada40f7af8a
Hejiko Hejio <hejyll@gmail.com>
2022-11-14 00:01:29 +0900

    Use jpradio.Jpradio

--- src/jpradio_recorder/recorder.py
+++ src/jpradio_recorder/recorder.py
@@ -4,8 +4,7 @@ import os
 from typing import Any, Dict, List, Optional
 
 import tqdm
-from jpradio import Hibiki, Onsen, Program, Radiko, Station
-from jpradio.platforms.base import Platform
+from jpradio import Jpradio, Program, Station
 
 from .database import Database
 from .query import ProgramQuery, ProgramQueryList
@@ -27,60 +26,6 @@ class RecordedProgram(Program):
         }
 
 
-def _get_all_platforms_cls() -> List[Platform]:
-    return [Radiko, Onsen, Hibiki]
-
-
-class RadioPlatforms(Platform):
-    def __init__(self, configs: Dict[str, Any]) -> None:
-        super().__init__()
-        all_platform_cls = _get_all_platforms_cls()
-        self._platforms = {
-            cls.id: cls(**configs.get(cls.id, {})) for cls in all_platform_cls
-        }
-        self._station_id_platform_map = {
-            station.id: self._platforms[station.platform_id]
-            for station in self.get_stations()
-        }
-
-    def id(self, program: Program) -> str:
-        return self.get_platform(program).id
-
-    def name(self, program: Program) -> str:
-        return self.get_platform(program).name
-
-    def ascii_name(self, program: Program) -> str:
-        return self.get_platform(program).ascii_name
-
-    def url(self, program: Program) -> str:
-        return self.get_platform(program).url
-
-    def login(self) -> None:
-        for platform in self._platforms.values():
-            platform.login()
-
-    def close(self) -> None:
-        for platform in self._platforms.values():
-            platform.close()
-
-    def get_platform(self, program: Program) -> Platform:
-        return self._station_id_platform_map[program.station_id]
-
-    def get_stations(self) -> List[Station]:
-        return sum([p.get_stations() for p in self._platforms.values()], [])
-
-    def get_programs(
-        self, filters: Optional[List[str]] = None, **kwargs
-    ) -> List[Program]:
-        return sum([p.get_programs(filters) for p in self._platforms.values()], [])
-
-    def download_media(self, program: Program, filename: str) -> None:
-        self.get_platform(program).download_media(program, filename)
-
-    def get_default_filename(self, program: Program) -> str:
-        return self.get_platform(program).get_default_filename(program)
-
-
 class Recorder:
     def __init__(
         self,
@@ -90,7 +35,7 @@ class Recorder:
     ) -> None:
         self._media_root = media_root
 
-        self._platforms = RadioPlatforms(platform_config)
+        self._platforms = Jpradio(platform_config)
         self._database = Database(database_host)
 
     @property
