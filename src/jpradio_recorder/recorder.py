a5246a1ced182ca6a193850f730487a290205684
Hejiko Hejio <hejyll@gmail.com>
2022-11-13 23:19:43 +0900

    Add RadioPlatforms

--- src/jpradio_recorder/recorder.py
+++ src/jpradio_recorder/recorder.py
@@ -27,6 +27,60 @@ class RecordedProgram(Program):
         }
 
 
+def _get_all_platforms_cls() -> List[Platform]:
+    return [Radiko, Onsen, Hibiki]
+
+
+class RadioPlatforms(Platform):
+    def __init__(self, configs: Dict[str, Any]) -> None:
+        super().__init__()
+        all_platform_cls = _get_all_platforms_cls()
+        self._platforms = {
+            cls.id: cls(**configs.get(cls.id, {})) for cls in all_platform_cls
+        }
+        self._station_id_platform_map = {
+            station.id: self._platforms[station.platform_id]
+            for station in self.get_stations()
+        }
+
+    def id(self, program: Program) -> str:
+        return self.get_platform(program).id
+
+    def name(self, program: Program) -> str:
+        return self.get_platform(program).name
+
+    def ascii_name(self, program: Program) -> str:
+        return self.get_platform(program).ascii_name
+
+    def url(self, program: Program) -> str:
+        return self.get_platform(program).url
+
+    def login(self) -> None:
+        for platform in self._platforms.values():
+            platform.login()
+
+    def close(self) -> None:
+        for platform in self._platforms.values():
+            platform.close()
+
+    def get_platform(self, program: Program) -> Platform:
+        return self._station_id_platform_map[program.station_id]
+
+    def get_stations(self) -> List[Station]:
+        return sum([p.get_stations() for p in self._platforms.values()], [])
+
+    def get_programs(
+        self, filters: Optional[List[str]] = None, **kwargs
+    ) -> List[Program]:
+        return sum([p.get_programs(filters) for p in self._platforms.values()], [])
+
+    def download_media(self, program: Program, filename: str) -> None:
+        self.get_platform(program).download_media(program, filename)
+
+    def get_default_filename(self, program: Program) -> str:
+        return self.get_platform(program).get_default_filename(program)
+
+
 class Recorder:
     def __init__(
         self,
@@ -36,10 +90,7 @@ class Recorder:
     ) -> None:
         self._media_root = media_root
 
-        platform_classes: List[Platform] = [Radiko, Onsen, Hibiki]
-        self._platforms = {
-            cls.id: cls(**platform_config.get(cls.id, {})) for cls in platform_classes
-        }
+        self._platforms = RadioPlatforms(platform_config)
         self._database = Database(database_host)
 
     @property
@@ -47,17 +98,21 @@ class Recorder:
         return self._database
 
     def __enter__(self) -> "Recorder":
-        for platform in self._platforms.values():
-            platform.login()
+        self.login()
         return self
 
     def __exit__(self, exc_type, exc_value, traceback) -> None:
-        for platform in self._platforms.values():
-            platform.close()
+        self.close()
+
+    def login(self) -> None:
+        self._platforms.login()
+
+    def close(self) -> None:
+        self._platforms.close()
         self.db.close()
 
     def fetch_stations(self) -> None:
-        stations = sum([p.get_stations() for p in self._platforms.values()], [])
+        stations = self._platforms.get_stations()
         self.db.stations.delete_many({})
         self.db.stations.insert_many([s.to_dict() for s in stations])
 
@@ -80,7 +135,7 @@ class Recorder:
             return
 
         logger.info("Start fetching programs")
-        programs = sum([p.get_programs() for p in self._platforms.values()], [])
+        programs = self._platforms.get_programs()
         logger.info(f"Finish fetching {len(programs)} programs")
         self.db.reset_fetched_programs()
         self.db.fetched_programs.insert_many([p.to_dict() for p in programs])
@@ -101,10 +156,6 @@ class Recorder:
 
     def record_programs(self) -> List[RecordedProgram]:
         logger.info("Start recording programs")
-        station_id_platform_map = {
-            station.id: self._platforms[station.platform_id]
-            for station in self.get_stations()
-        }
 
         # Only programs that have completed broadcasting can be downloaded.
         date_query = ProgramQuery(
@@ -117,17 +168,16 @@ class Recorder:
             target_id = program.pop("_id")
             if not self.db.recorded_programs.find_one(program):
                 program = Program.from_dict(program)
-                platform = station_id_platform_map[program.station_id]
                 filename = os.path.join(
                     self._media_root,
-                    platform.id,
+                    self._platforms.id(program),
                     program.station_id,
                     fix_to_path(program.name),
-                    platform.get_default_filename(program),
+                    self._platforms.get_default_filename(program),
                 )
                 os.makedirs(os.path.dirname(filename), exist_ok=True)
                 try:
-                    platform.download(program, filename)
+                    self._platforms.download(program, filename)
                     recorded = RecordedProgram.from_dict(
                         {
                             **program.to_dict(),
