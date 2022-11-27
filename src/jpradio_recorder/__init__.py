ed06ba0b17cb7b42f4632d5639ce4eca564f861c
Hejiko Hejio <hejyll@gmail.com>
2022-11-27 19:07:35 +0900

    Remove query.py and use jpradio.ProgramQuery{,List}

--- src/jpradio_recorder/__init__.py
+++ src/jpradio_recorder/__init__.py
@@ -1,4 +1,3 @@
 from ._version import __version__
 from .database import Database  # NOQA
-from .query import ProgramQuery, ProgramQueryList  # NOQA
 from .recorder import RecordedProgram, Recorder  # NOQA
