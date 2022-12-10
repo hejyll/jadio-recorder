88f59cafc73965b7a92aab267c19dac83f1b0e2b
Hejiko Hejio <hejyll@gmail.com>
2022-12-10 23:17:08 +0900

    Add stations and pagenated programs APIs

--- src/jadio_recorder/api.py
+++ src/jadio_recorder/api.py
@@ -1,13 +1,14 @@
 import argparse
 import datetime
+from enum import Enum
 from typing import Any, Dict
 
-from flask import Flask, jsonify
-
-from jadio import Program
+from flask import Flask, jsonify, request
 
 # from .database import Database
 from jadio_recorder.database import Database
+
+DEFAULT_ITEMS_PER_PAGE = 20
 
 
 app = Flask(__name__)
@@ -16,43 +17,77 @@ app.config["JSON_AS_ASCII"] = False
 __args: argparse.Namespace
 
 
+class ProgramStatus(Enum):
+    FETCHED = "fetched"
+    RESERVED = "reserved"
+    RECORDED = "recorded"
+
+
 def _fix_db_data(data: Dict[str, Any]) -> Dict[str, Any]:
-    data.pop("_id", None)
-    data.pop("raw_data", None)
     for key, value in data.items():
         if isinstance(value, datetime.datetime):
             data[key] = value.strftime("%Y-%m-%d %H:%M")
     return data
 
 
-@app.route("/fetched")
-def fetched():
+@app.route("/stations", methods=["GET"])
+def stations():
     with Database(host=__args.database_host) as db:
-        programs = db.fetched_programs.find({})
-        ret = [_fix_db_data(program) for program in programs[:10]]
-    return jsonify(ret)
+        stations = list(db.stations.find({}, projection={"_id": False}))
+    return jsonify(stations)
 
 
-@app.route("/reserved")
-def reserved():
+@app.route("/programs", methods=["GET"])
+def programs():
+    # parse query parameters
+    args = request.args
+    page = args.get("page", default=0, type=int)
+    items = args.get("items", default=DEFAULT_ITEMS_PER_PAGE, type=int)
+    status = args.get("status", default=ProgramStatus.RECORDED.value, type=str)
+    status = ProgramStatus(status)
+
+    # construct arguments for Mongo find
+    sort_key = "recorded_datetime" if status == ProgramStatus.RECORDED else "datetime"
+    find_args = dict(
+        filter={},
+        projection={"_id": False, "raw_data": False},
+        sort=[(sort_key, -1)],
+        skip=items * page,
+        limit=items,
+    )
+
+    # pickup programs
+    programs = []
     with Database(host=__args.database_host) as db:
-        programs = db.reserved_programs.find({})
-        ret = [_fix_db_data(program) for program in programs[:10]]
-    return jsonify(ret)
+        if status == ProgramStatus.FETCHED:
+            programs += db.fetched_programs.find(**find_args)
+            for program in programs:
+                find_filter = {
+                    "station_id": program["station_id"],
+                    "episode_id": program["episode_id"],
+                }
+                if db.recorded_programs.find_one(find_filter):
+                    program["status"] = ProgramStatus.RECORDED.value
+                elif db.reserved_programs.find_one(find_filter):
+                    program["status"] = ProgramStatus.RESERVED.value
+                else:
+                    program["status"] = ProgramStatus.FETCHED.value
+        elif status == ProgramStatus.RESERVED:
+            programs += db.reserved_programs.find(**find_args)
+            for program in programs:
+                program["status"] = ProgramStatus.RESERVED.value
+        elif status == ProgramStatus.RECORDED:
+            programs += db.recorded_programs.find(**find_args)
+            for program in programs:
+                program["status"] = ProgramStatus.RECORDED.value
+    programs = [_fix_db_data(program) for program in programs]
+    return jsonify(programs)
 
 
-@app.route("/recorded")
-def recorded():
-    with Database(host=__args.database_host) as db:
-        programs = db.recorded_programs.find({})
-        ret = [_fix_db_data(program) for program in programs[:10]]
-    return jsonify(ret)
-
-
-@app.route("/timestamp")
+@app.route("/timestamp", methods=["GET"])
 def timestamp():
     with Database(host=__args.database_host) as db:
-        timestamps = db.timestamp.find({})
+        timestamps = db.timestamp.find({}, projection={"_id": False})
         ret = [_fix_db_data(timestamp) for timestamp in timestamps[:10]]
     return jsonify(ret)
 
@@ -98,4 +133,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
