fe047cec54542afee27baf772b1764b59b83d297
Hejiko Hejio <hejyll@gmail.com>
2024-06-07 16:50:57 +0900

    fix --database-host in docker/jadio-recorder.sh

--- docker/jadio-recorder.sh
+++ docker/jadio-recorder.sh
@@ -4,4 +4,4 @@ jadio-recorder \
     --queries-path /data/jadio-recorder/queries.json \
     --media-root /data/media \
     --platform-config-path /data/jadio-recorder/config.json \
-    --database-host "mongodb://docker_mongo_1:27017/"
+    --database-host "${MONGO_HOST}"
