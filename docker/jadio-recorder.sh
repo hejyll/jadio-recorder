0be20264c7cc84dded02821eb7c79abf0be4f39a
Hejiko Hejio <hejyll@gmail.com>
2024-06-08 21:05:08 +0900

    bugfix: periodic recording with cron in docker container

--- docker/jadio-recorder.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-jadio-recorder \
-    --queries-path /data/jadio-recorder/queries.json \
-    --media-root /data/media \
-    --platform-config-path /data/jadio-recorder/config.json \
-    --database-host "${MONGO_HOST}"
