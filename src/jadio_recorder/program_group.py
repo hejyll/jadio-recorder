b720f4e540981c3f6e034c8a9fcfa2418a190916
Hejiko Hejio <hejyll@gmail.com>
2024-06-12 08:29:44 +0900

    add program_group.py instead of configs.py

---
+++ src/jadio_recorder/program_group.py
@@ -0,0 +1,50 @@
+from __future__ import annotations
+
+from dataclasses import dataclass
+from typing import List, Optional, Union
+
+from dataclasses_json import DataClassJsonMixin
+from dataclasses_json.core import Json
+
+from .program_category import ProgramCategory
+from .program_query import ProgramQuery
+
+
+@dataclass
+class ProgramGroup(DataClassJsonMixin):
+    """Represents a grouping of radio programs.
+
+    Attributes:
+        query (`ProgramQuery`): Query for the program to be grouped.
+        title (str): Title of the program group.
+        description (str): Description of the program group.
+        category (str or `ProgramCategory` or list): Category of the program
+            group. Multiple categories can be specified.
+        tags (str or list): Any tag attached to the program group.
+        copyright (str): Copyright of the program group.
+        link_url (str): URL of the program group link.
+        image_url (str): URL of the program group image.
+        author (str): Author of the program group.
+        enable_record (bool): Whether to record programs that correspond to
+            the program group or not.
+        enable_feed (bool): Whether to feed the program group or not.
+    """
+
+    query: ProgramQuery
+    title: Optional[str] = None
+    description: Optional[str] = None
+    category: Optional[Union[str, ProgramCategory, List[ProgramCategory]]] = None
+    tags: Optional[Union[str, List[str]]] = None
+    copyright: Optional[str] = None
+    link_url: Optional[str] = None
+    image_url: Optional[str] = None
+    author: Optional[str] = None
+    enable_record: bool = True
+    enable_feed: bool = True
+
+    @classmethod
+    def from_dict(cls, kvs: Json, *, infer_missing: bool = False) -> ProgramGroup:
+        ret = super().from_dict(kvs, infer_missing=infer_missing)
+        if ret.category is not None:
+            ret.category = ProgramCategory.from_value(ret.category)
+        return ret
